c9f56712290d66a028f7660816b80a886cc3be17
sean21502 <seanhite@gmail.com>
2017-05-17 18:07:44 -0400

    checkpoint 10

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -12,11 +12,20 @@
         templateUrl: '/templates/directives/seek_bar.html',
         replace: true,
         restrict: 'E',
-        scope: { },
+        scope: {
+        onChange: '&'
+     	},
         link: function(scope, element, attributes) {
         	scope.value = 0;
             scope.max = 100;
  			var seekBar = $(element);
+			attributes.$observe('value', function(newValue) {
+     			scope.value = newValue;
+ 			});
+			
+ 			attributes.$observe('max', function(newValue) {
+				scope.max = newValue;
+ 			});
             var percentString = function () {
             	var value = scope.value;
                 var max = scope.max;
@@ -30,6 +39,7 @@
 				scope.onClickSeekBar = function(event) {
              		var percent = calculatePercent(seekBar, event);
              		scope.value = percent * scope.max;
+					notifyOnChange(scope.value);
          		};
 			
 				scope.trackThumb = function() {
@@ -37,8 +47,15 @@
          			var percent = calculatePercent(seekBar, event);
          			scope.$apply(function() {
              			scope.value = percent * scope.max;
+						notifyOnChange(scope.value);
          			});
      			});
+					
+				var notifyOnChange = function(newValue) {
+     				if (typeof scope.onChange === 'function') {
+         			scope.onChange({value: newValue});
+     				}
+ 				};
  
      				$document.bind('mouseup.thumb', function() {
          			$document.unbind('mousemove.thumb');
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-	function SongPlayer(Fixtures) {
+	function SongPlayer($rootScope, Fixtures) {
 		var SongPlayer = {};
 		var currentAlbum = Fixtures.getAlbum();
 		 
@@ -9,6 +9,14 @@
 		};
 		
 		SongPlayer.currentSong = null;
+		
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+		
+ 		SongPlayer.currentTime = null;
+		
 /**
 * @desc Buzz object audio file
 * @type {Object}
@@ -31,6 +39,12 @@
         		formats: ['mp3'],
         		preload: true
     		});
+			
+			currentBuzzObject.bind('timeupdate', function() {
+         		$rootScope.$apply(function() {
+            		SongPlayer.currentTime = currentBuzzObject.getTime();
+         		});
+     		});
  
     		SongPlayer.currentSong = song;
  			};
@@ -112,12 +126,23 @@
 				
      		}	
  		};
+		
+/**
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+		SongPlayer.setCurrentTime = function(time) {
+     		if (currentBuzzObject) {
+         	currentBuzzObject.setTime(time);
+     		}
+ 		};
 
 		return SongPlayer;
 	}
  
      angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures' , SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
 
